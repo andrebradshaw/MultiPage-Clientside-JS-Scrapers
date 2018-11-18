008372384bce0060fe381f447abf5fb564ce2d73
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-17 20:48:19 -0500

    Update meetup.js

--- meetup.js
+++ meetup.js
@@ -1,90 +1,12 @@
 /*/ watch the live build: 
 https://youtu.be/4MVhm6vlRpQ
 /*/
+function checker(elm, type) {  if (elm != undefined) {    if (type == 'href') {      return elm.href;    }    if (type == 'text') {      return elm.innerText;    }    if (type == 'next') {      return elm;    }  } else {    return '';  }}
 
-function checker(elm, type) {
-  if (elm != undefined) {
-    if (type == 'href') {
-      return elm.href;
-    }
-    if (type == 'text') {
-      return elm.innerText;
-    }
-    if (type == 'next') {
-      return elm;
-    }
-  } else {
-    return '';
-  }
-}
+function reg(elm, n) {  if (elm != null) {    return elm[n];  } else {    return '';  }}
 
-function reg(elm, n) {
-  if (elm != null) {
-    return elm[n];
-  } else {
-    return '';
-  }
-}
+function getApprxLastVisit(elm) {  var oneday = 86400000;  var str = reg(/(?<=Last visited ).+/.exec(elm), 0);  var now = new Date().getTime();  if (/today/.test(str) === true) {    return new Date(now - (oneday));  }  if (/past few days/.test(str) === true) {    return new Date(now - (oneday * 3));  }  if (/a week ago/.test(str) === true) {    return new Date(now - (oneday * 7));  }  if (/past two weeks/.test(str) === true) {    return new Date(now - (oneday * 14));  }  if (/over two weeks ago/.test(str) === true) {    return new Date(now - (oneday * 21));  }  if (/over a month ago/.test(str) === true) {    return new Date(now - (oneday * 40));  }  if (/over two months ago/.test(str) === true) {    return new Date(now - (oneday * 70));  }  if (/over three months ago/.test(str) === true) {    return new Date(now - (oneday * 100));  }  if (/over four months ago/.test(str) === true) {    return new Date(now - (oneday * 130));  }  if (/over five months ago/.test(str) === true) {    return new Date(now - (oneday * 160));  }  if (/over six months ago/.test(str) === true) {    return new Date(now - (oneday * 190));  }  if (/over seven months ago/.test(str) === true) {    return new Date(now - (oneday * 220));  }  if (/over seven months ago/.test(str) === true) {    return new Date(now - (oneday * 220));  }  if (/over eight months ago/.test(str) === true) {    return new Date(now - (oneday * 250));  }  if (/over nine months ago/.test(str) === true) {    return new Date(now - (oneday * 280));  }  if (/over ten months ago/.test(str) === true) {    return new Date(now - (oneday * 310));  }  if (/over eleven months ago/.test(str) === true) {    return new Date(now - (oneday * 340));  }  if (/over a year ago/.test(str) === true) {    return new Date(now - (oneday * 370));  }}
 
-function getApprxLastVisit(elm){
-	var oneday = 86400000;
-	var str = reg(/(?<=Last visited ).+/.exec(elm),0);
-	var now = new Date().getTime();
-	if(/today/.test(str) === true){
-		return new Date(now-(oneday));
-	}
-	if(/past few days/.test(str) === true){
-		return new Date(now-(oneday*3));
-	}
-	if(/a week ago/.test(str) === true){
-		return new Date(now-(oneday*7));
-	}
-	if(/past two weeks/.test(str) === true){
-		return new Date(now-(oneday*14));
-	}
-	if(/over two weeks ago/.test(str) === true){
-		return new Date(now-(oneday*21));
-	}
-	if(/over a month ago/.test(str) === true){
-		return new Date(now-(oneday*40));
-	}
-	if(/over two months ago/.test(str) === true){
-		return new Date(now-(oneday*70));
-	}
-	if(/over three months ago/.test(str) === true){
-		return new Date(now-(oneday*100));
-	}
-	if(/over four months ago/.test(str) === true){
-		return new Date(now-(oneday*130));
-	}
-	if(/over five months ago/.test(str) === true){
-		return new Date(now-(oneday*160));
-	}
-	if(/over six months ago/.test(str) === true){
-		return new Date(now-(oneday*190));
-	}
-	if(/over seven months ago/.test(str) === true){
-		return new Date(now-(oneday*220));
-	}
-	if(/over seven months ago/.test(str) === true){
-		return new Date(now-(oneday*220));
-	}
-	if(/over eight months ago/.test(str) === true){
-		return new Date(now-(oneday*250));
-	}
-	if(/over nine months ago/.test(str) === true){
-		return new Date(now-(oneday*280));
-	}
-	if(/over ten months ago/.test(str) === true){
-		return new Date(now-(oneday*310));
-	}
-	if(/over eleven months ago/.test(str) === true){
-		return new Date(now-(oneday*340));
-	}
-	if(/over a year ago/.test(str) === true){
-		return new Date(now-(oneday*370));
-	}
-}
 var pagesArray = [];
 var memberLinksArray = [];
 var memberDetailsArray = [];
@@ -96,7 +18,6 @@ for (p = 0; p < numberOfPages; p++) {
   pagesArray.push(basePage + '?offset=' + (p * 20) + '&sort=social&desc=1');
 }
 
-
 function getMemberLinks(pageLink, n) {
   setTimeout(() => {
     var pwin = window.open(pageLink)
@@ -106,9 +27,9 @@ function getMemberLinks(pageLink, n) {
         var memberList = containedList.getElementsByClassName('clearfix line memberInfo');
         for (i = 0; i < memberList.length; i++) {
           var itm = checker(memberList[i].getElementsByTagName('a')[0], 'href');
-		  var lastvisit = checker(memberList[i].getElementsByClassName('last')[0], 'text');
+          var lastvisit = checker(memberList[i].getElementsByClassName('last')[0], 'text');
 
-          memberLinksArray.push([itm,getApprxLastVisit(lastvisit)]);
+          memberLinksArray.push([itm, getApprxLastVisit(lastvisit)]);
         }
       }
     }, 4500);
@@ -125,7 +46,7 @@ function runPages(arr) {
 }
 
 var memberScrape_waitTime = (numberOfPages * 5050);
-alert('just a heads up, this is going to take ' + ((numberOfMembers * 5)/60) +' minutes to complete.');
+alert('just a heads up, this is going to take ' + ((numberOfMembers * 5) / 60) + ' minutes to complete.');
 
 var runfirst = new Promise(res => {
   res(runPages(pagesArray));
@@ -133,7 +54,7 @@ var runfirst = new Promise(res => {
 
 function getProfileData(links, t) {
   setTimeout(() => {
-    function getProfile(link, n) {
+    function getProfile(link, n, end) {
       setTimeout(() => {
         var wnd = window.open(link[0]);
         setTimeout(() => {
@@ -181,8 +102,8 @@ function getProfileData(links, t) {
             var contArr = [];
             if (qq.length > 1) {
               for (i = 1; i < qq.length; i++) {
-                let q = checker(qq[i],'text');
-                let a = checker(aa[i],'text');
+                let q = checker(qq[i], 'text');
+                let a = checker(aa[i], 'text');
                 contArr.push([q, a]);
               }
             }
@@ -191,8 +112,8 @@ function getProfileData(links, t) {
 
           var personObj = {
             'name': fullname,
-			'profile': link[0],
-			'activedate': link[1],
+            'profile': link[0],
+            'activedate': link[1],
             'geo': answer[0].innerText,
             'groups': getGroups(groupContainer),
             'interests': getInterests(),
@@ -201,28 +122,35 @@ function getProfileData(links, t) {
           }
           memberDetailsArray.push(personObj);
         }, 4500);
-		setTimeout(() => {
-			wnd.close();
+        setTimeout(() => {
+          wnd.close();
         }, 4950);
+
+        if (n > (end - 2)) {
+          dl(reg(/(?<=meetup\.com\/).+?(?=\/)/.exec(window.location.href), 0) + '_members.json', JSON.stringify(memberDetailsArray));
+        }
+
       }, ((n + 1) * 5000));
     }
-	for(y=0; y<links.length; y++){
-		getProfile(links[y], y);
-	}
+    for (y = 0; y < links.length; y++) {
+      getProfile(links[y], y, links.length);
+    }
 
   }, t);
 }
+
 function dl(filename, text) {
-setTimeout(()=>{
-  var elmi = document.createElement('a');
-  elmi.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  elmi.setAttribute('download', filename);
-  elmi.style.display = 'none';
-  document.body.appendChild(elmi);
-  elmi.click();
-  document.body.removeChild(elmi);
-}, (numberOfMembers * 5050));
+  setTimeout(() => {
+    console.log('running profile pages');
+    var elmi = document.createElement('a');
+    elmi.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    elmi.setAttribute('download', filename);
+    elmi.style.display = 'none';
+    document.body.appendChild(elmi);
+    elmi.click();
+    document.body.removeChild(elmi);
+  }, 22000);
 }
 var namethis = /(?<=org\/).+?(?=\/)/.exec(window.location.href);
 
-runfirst.then(getProfileData(memberLinksArray, memberScrape_waitTime)).then(dl(reg(/(?<=meetup\.com\/).+?(?=\/)/.exec(window.location.href), 0)+"_members.json", JSON.stringify(memberDetailsArray)));
+runfirst.then(getProfileData(memberLinksArray, memberScrape_waitTime));
